e28894b383d5591f13e5618ca56fa5a45058c8f7
Shun Kakinoki <shunkakinoki@gmail.com>
2020-08-27 14:38:35 +0900

    perf: ⚡️ ini stats number fields

--- packages/common/src/serializers/Space.ts
+++ packages/common/src/serializers/Space.ts
@@ -2,6 +2,7 @@
 
 import serializeFirebaseDate from "@sentrei/common/serializers/Date";
 import Space from "@sentrei/types/models/Space";
+import Stats from "@sentrei/types/models/Stats";
 
 export const serializeSpace = (
   snap: firebase.firestore.DocumentSnapshot<Space.Response>,
@@ -10,10 +11,7 @@ export const serializeSpace = (
 
   return {
     ...data,
-    inviteCount: data.inviteCount as number,
-    memberCount: data.memberCount as number,
-    roomCount: data.roomCount as number,
-    scoreCount: data.scoreCount as number,
+    stats: data.stats as Stats.NumberFields,
     createdAt: serializeFirebaseDate(data.createdAt),
     id: snap.id,
     updatedAt: serializeFirebaseDate(data.updatedAt),
@@ -27,10 +25,7 @@ export const serializeAdminSpace = (
 
   return {
     ...data,
-    inviteCount: data.inviteCount as number,
-    memberCount: data.memberCount as number,
-    roomCount: data.roomCount as number,
-    scoreCount: data.scoreCount as number,
+    stats: data.stats as Stats.NumberFields,
     createdAt: serializeFirebaseDate(data.createdAt),
     id: snap.id,
     updatedAt: serializeFirebaseDate(data.updatedAt),
