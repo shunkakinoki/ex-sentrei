040d40f7f2f50608b30ba9a2868e7d1130a04a4a
Shun Kakinoki <shunkakinoki@gmail.com>
2020-09-08 14:31:38 +0900

    feat: 🎸 complete refactor common utils sentry

--- packages/web/pages_/_app.tsx
+++ packages/web/pages_/_app.tsx
@@ -1,9 +1,6 @@
 import CssBaseline from "@material-ui/core/CssBaseline";
 import {ThemeProvider as MaterialThemeProvider} from "@material-ui/core/styles";
-import * as Sentry from "@sentry/browser";
-import {RewriteFrames} from "@sentry/integrations";
 import {AppProps} from "next/app";
-import getConfig from "next/config";
 import Head from "next/head";
 import * as React from "react";
 import {RecoilRoot} from "recoil";
@@ -23,24 +20,9 @@ import Backdrop from "@sentrei/ui/components/Backdrop";
 import Snackbar from "@sentrei/ui/components/Snackbar";
 
 import "@sentrei/common/utils/nprogress";
+import "@sentrei/common/utils/sentry";
 import "@sentrei/web/styles/global.scss";
 import "@sentrei/web/styles/nprogress.scss";
-
-const config = getConfig();
-const distDir = `${config.serverRuntimeConfig.rootDir}/.next`;
-Sentry.init({
-  enabled: true,
-  integrations: [
-    new RewriteFrames({
-      iteratee: (frame: Sentry.StackFrame): Sentry.StackFrame => {
-        // eslint-disable-next-line no-param-reassign
-        frame.filename = frame?.filename?.replace(distDir, "app:///_next");
-        return frame;
-      },
-    }),
-  ],
-  dsn: process.env.SENTRY_DSN,
-});
 
 const CustomApp = ({Component, pageProps}: AppProps): JSX.Element => {
   const [user, setUser] = React.useState<User.Get | null | undefined>(
