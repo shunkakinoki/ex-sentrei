9ea938979262c19fbbcf24e72f223fbc24bbf15c
Shun Kakinoki <shunkakinoki@gmail.com>
2020-09-11 05:29:28 +0900

    feat: 🎸 ini video theme

--- packages/video/src/VideoApp.tsx
+++ packages/video/src/VideoApp.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import {ThemeProvider} from "@material-ui/styles";
+import Paper from "@material-ui/core/Paper";
 
 import App from "@sentrei/video/App";
 import {useAppState} from "@sentrei/video/state";
@@ -7,18 +9,23 @@ import "@sentrei/video/types";
 import {VideoProvider} from "@sentrei/video/components/VideoProvider";
 import useConnectionOptions from "@sentrei/video/utils/useConnectionOptions/useConnectionOptions";
 import UnsupportedBrowserWarning from "@sentrei/video/components/UnsupportedBrowserWarning/UnsupportedBrowserWarning";
+import VideoTheme from "@sentrei/common/containers/VideoTheme";
 
 const VideoApp = () => {
   const {error, setError} = useAppState();
   const connectionOptions = useConnectionOptions();
 
   return (
-    <UnsupportedBrowserWarning>
-      <VideoProvider options={connectionOptions} onError={setError}>
-        <ErrorDialog dismissError={() => setError(null)} error={error} />
-        <App />
-      </VideoProvider>
-    </UnsupportedBrowserWarning>
+    <ThemeProvider theme={VideoTheme}>
+      <Paper>
+        <UnsupportedBrowserWarning>
+          <VideoProvider options={connectionOptions} onError={setError}>
+            <ErrorDialog dismissError={() => setError(null)} error={error} />
+            <App />
+          </VideoProvider>
+        </UnsupportedBrowserWarning>
+      </Paper>
+    </ThemeProvider>
   );
 };
 
