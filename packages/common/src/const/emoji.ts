5f621fa7ea4f63e4beb00ebda81f88599411ea08
Shun Kakinoki <shunkakinoki@gmail.com>
2020-09-11 07:26:43 +0900

    perf: ⚡️ ini const emoji

--- packages/common/src/const/emoji.ts
+++ packages/common/src/const/emoji.ts
@@ -1,3 +1,9 @@
-export const RoomEmoji = ":sushi:";
+const UserEmojiArray = [":joy:", ":hushed:", ":neutral_face:"];
 
-export const UserEmoji = ":blush:";
+const UserRandomIndex = Math.floor(Math.random() * UserEmojiArray.length);
+export const UserEmoji = UserEmojiArray[UserRandomIndex];
+
+const RoomEmojiArray = [":sushi:", ":ice_cream:", ":ramen:"];
+
+const RoomRandomIndex = Math.floor(Math.random() * RoomEmojiArray.length);
+export const RoomEmoji = RoomEmojiArray[RoomRandomIndex];
