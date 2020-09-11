74504c717bc9eaaa806c1f3d57790e6287d7bb82
Shun Kakinoki <shunkakinoki@gmail.com>
2020-09-11 14:52:19 +0900

    perf: ⚡️ refactor auth form login grid

--- packages/ui/src/components/AuthFormLoginGrid/AuthFormLoginGrid.tsx
+++ packages/ui/src/components/AuthFormLoginGrid/AuthFormLoginGrid.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 
 import MuiLink from "@sentrei/ui/components/MuiLink";
 
-export default function AuthFormSignupGrid(): JSX.Element {
+export default function AuthFormLoginGrid(): JSX.Element {
   const {t} = useTranslation();
 
   return (
