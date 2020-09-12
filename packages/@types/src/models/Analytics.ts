147956de231bd3321ddbd1ebba5f630a9fe1641d
Shun Kakinoki <shunkakinoki@gmail.com>
2020-09-12 15:25:35 +0900

    feat: 🎸 ini analytics model

---
+++ packages/@types/src/models/Analytics.ts
@@ -0,0 +1,28 @@
+import Actions from "@sentrei/types/models/Actions";
+import Metrics from "@sentrei/types/models/Metrics";
+import Stats from "@sentrei/types/models/Stats";
+
+declare namespace Analytics {
+  export type Types = "hour" | "day" | "week";
+
+  interface Fields {
+    actions: Actions.NumberFields;
+    stats: Stats.NumberFields;
+    metrics: Metrics.NumberFields;
+    type: Types;
+  }
+
+  export type Create = Fields;
+
+  export type Response = Fields;
+
+  export interface Get extends Fields {
+    id: string;
+  }
+
+  export interface Snapshot extends Get {
+    snap: firebase.firestore.DocumentSnapshot;
+  }
+}
+
+export default Analytics;
