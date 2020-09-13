12460aa8c8de34eef6a68fcc8a1619935ecc3d7d
Shun Kakinoki <shunkakinoki@gmail.com>
2020-09-13 13:07:21 +0900

    perf: ⚡️ refactor stats model

--- packages/@types/src/models/Stats.ts
+++ packages/@types/src/models/Stats.ts
@@ -15,23 +15,21 @@ export const rootCollection: RootCollection[] = [
   "users",
 ];
 
-export type RoomCollection = "participants";
+export type RoomCollection = "analytics" | "participants" | "sessions";
 
-export const roomCollection: RoomCollection[] = ["participants"];
+export const roomCollection: RoomCollection[] = [
+  "analytics",
+  "participants",
+  "sessions",
+];
 
-export type SpaceCollection =
-  | "activity"
-  | "invites"
-  | "members"
-  | "rooms"
-  | "sessions";
+export type SpaceCollection = "activity" | "invites" | "members" | "rooms";
 
 export const spaceCollection: SpaceCollection[] = [
   "activity",
   "invites",
   "members",
   "rooms",
-  "sessions",
 ];
 
 export type RoomSpaceCollection = RoomCollection | SpaceCollection;
