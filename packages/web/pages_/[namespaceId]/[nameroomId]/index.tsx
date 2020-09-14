31ad1265372c6597bc2e3901fa254cc79cf04684
Shun Kakinoki <shunkakinoki@gmail.com>
2020-09-14 16:33:44 +0900

    feat: 🎸 update roomId for app index pages

--- packages/web/pages_/[namespaceId]/[nameroomId]/index.tsx
+++ packages/web/pages_/[namespaceId]/[nameroomId]/index.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 
 import AuthContext from "@sentrei/common/context/AuthContext";
 import {getMembers} from "@sentrei/common/firebase/members";
+import {getNameroom} from "@sentrei/common/firebase/namerooms";
 import {getNamespace} from "@sentrei/common/firebase/namespaces";
 import Member from "@sentrei/types/models/Member";
 import HomeScreen from "@sentrei/ui/components/HomeScreen";
@@ -13,6 +14,7 @@ const NameroomId = (): JSX.Element => {
   const {query} = useRouter();
 
   const {user, profile} = React.useContext(AuthContext);
+  const [roomId, setRoomId] = React.useState<string | null | undefined>();
   const [spaceId, setSpaceId] = React.useState<string | null | undefined>();
   const [members, setMembers] = React.useState<
     Member.Get[] | null | undefined
@@ -30,12 +32,31 @@ const NameroomId = (): JSX.Element => {
   }, [query.namespaceId]);
 
   React.useEffect(() => {
+    async function setRoom(): Promise<void> {
+      if (!spaceId) {
+        return;
+      }
+      const nameroom = await getNameroom(spaceId, String(query.nameroomId));
+      if (!nameroom) {
+        return;
+      }
+      setRoomId(nameroom.uid);
+    }
+    setRoom();
+  }, [query.nameroomId, spaceId]);
+
+  React.useEffect(() => {
     if (spaceId) {
       getMembers({spaceId}).then(setMembers);
     }
   }, [spaceId]);
 
-  if (user === undefined || spaceId === undefined || members === undefined) {
+  if (
+    user === undefined ||
+    spaceId === undefined ||
+    roomId === undefined ||
+    members === undefined
+  ) {
     return (
       <>
         <SentreiAppHeader
@@ -50,7 +71,7 @@ const NameroomId = (): JSX.Element => {
     );
   }
 
-  if (!user || !profile || !spaceId || !members) {
+  if (!user || !profile || !spaceId || !roomId || !members) {
     return (
       <>
         <SentreiAppHeader
