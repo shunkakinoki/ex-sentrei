b7d2a044e1ff763ebe900bae745cabc8f4531098
Shun Kakinoki <shunkakinoki@gmail.com>
2020-09-16 11:57:29 +0900

    perf: ⚡️ ini bond focus work colors

--- packages/common/src/const/color.ts
+++ packages/common/src/const/color.ts
@@ -9,3 +9,7 @@ export const AnalyticsColor = "#8884d8";
 export const BadgeOnlineColor = "#44b700";
 export const BadgeOfflineColor = "#ea907a";
 export const BadgeAwayColor = "#ede682";
+
+export const BondColor = "#68b0ab";
+export const FocusColor = "#8fc0a9";
+export const WorkColor = "#c8d5b9";
