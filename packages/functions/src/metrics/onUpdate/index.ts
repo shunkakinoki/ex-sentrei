9684eeeba32da6250f8ce88801a6baa8db1bf21c
Shun Kakinoki <shunkakinoki@gmail.com>
2020-09-25 15:08:32 +0900

    perf: ⚡️ ini record index

--- packages/functions/src/metrics/onUpdate/index.ts
+++ packages/functions/src/metrics/onUpdate/index.ts
@@ -1,3 +1,7 @@
 export {default as durationBatchSet} from "./durationBatchSet";
 export {default as durationRootSet} from "./durationRootSet";
 export {default as durationUserSet} from "./durationUserSet";
+export {default as recordMemberSet} from "./recordMemberSet";
+export {default as recordRootSet} from "./recordRootSet";
+export {default as recordSpaceSet} from "./recordSpaceSet";
+export {default as recordUserSet} from "./recordUserSet";
