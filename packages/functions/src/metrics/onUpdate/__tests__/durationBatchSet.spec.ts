e477a3fa478a5bd1f887bb012dfbc8aa14514c4d
Shun Kakinoki <shunkakinoki@gmail.com>
2020-09-26 18:27:53 +0900

    test: 💍 remove unnecessary tests

--- packages/functions/src/metrics/onUpdate/__tests__/durationBatchSet.spec.ts
+++ packages/functions/src/metrics/onUpdate/__tests__/durationBatchSet.spec.ts
@@ -2,10 +2,7 @@ import * as admin from "firebase-admin";
 import functions from "firebase-functions-test";
 import {when} from "jest-when";
 
-import {
-  memberSessionResponse,
-  roomSessionResponse,
-} from "@sentrei/functions/__dummy__/Session";
+import {memberSessionResponse} from "@sentrei/functions/__dummy__/Session";
 import Metrics from "@sentrei/types/models/Metrics";
 import Session from "@sentrei/types/models/Session";
 
@@ -45,67 +42,6 @@ test("Invoke a request to add new analytics to member collection", async done =>
   const metricsData: Metrics.Update = {
     duration: 3,
     member: {
-      duration: 3,
-      focus: 3,
-    },
-    type: {
-      focus: 3,
-    },
-  };
-
-  expect(req).toBe(true);
-  expect(db.doc).toHaveBeenCalledWith("rooms/roomId/admin/metrics");
-  expect(db.doc).toHaveBeenCalledWith("spaces/spaceId/admin/metrics");
-  expect(db.doc).toHaveBeenCalledWith("users/userId/admin/metrics");
-  expect(db.doc).toHaveBeenCalledWith(
-    "spaces/spaceId/members/userId/admin/metrics",
-  );
-  expect(db.batch().set).toHaveBeenCalledWith("roomRef", metricsData, {
-    merge: true,
-  });
-  expect(db.batch().set).toHaveBeenCalledWith("spaceRef", metricsData, {
-    merge: true,
-  });
-  expect(db.batch().set).toHaveBeenCalledWith("userRef", metricsData, {
-    merge: true,
-  });
-  expect(db.batch().set).toHaveBeenCalledWith("memberRef", metricsData, {
-    merge: true,
-  });
-  done();
-});
-
-test("Invoke a request to add new analytics to room collection", async done => {
-  spyOn(db.batch(), "commit").and.returnValue(true);
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  when(db.doc as any)
-    .calledWith("rooms/roomId/admin/metrics")
-    .mockReturnValue("roomRef");
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  when(db.doc as any)
-    .calledWith("spaces/spaceId/admin/metrics")
-    .mockReturnValue("spaceRef");
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  when(db.doc as any)
-    .calledWith("users/userId/admin/metrics")
-    .mockReturnValue("userRef");
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  when(db.doc as any)
-    .calledWith("spaces/spaceId/members/userId/admin/metrics")
-    .mockReturnValue("memberRef");
-
-  const snap = {
-    after: {data: (): Session.Response => roomSessionResponse},
-  };
-  const wrapped = testEnv.wrap(durationBatchSet);
-  const req = await wrapped(snap);
-  const metricsData: Metrics.Update = {
-    duration: 3,
-    room: {
       duration: 3,
       focus: 3,
     },
