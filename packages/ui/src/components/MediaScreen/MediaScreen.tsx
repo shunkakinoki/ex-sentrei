d3465077767553503d33da63dc22d48f855cea1f
Shun Kakinoki <shunkakinoki@gmail.com>
2020-09-28 01:21:13 +0900

    perf: ⚡️ ini media screen

---
+++ packages/ui/src/components/MediaScreen/MediaScreen.tsx
@@ -0,0 +1,10 @@
+import Box from "@material-ui/core/Box";
+import * as React from "react";
+
+export default function MediaScreen(): JSX.Element {
+  return (
+    <>
+      <Box py={3} />
+    </>
+  );
+}
