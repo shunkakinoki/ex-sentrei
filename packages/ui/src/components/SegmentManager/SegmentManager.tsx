ecb50ff45823e207991069f22a204262d74b73e5
Shun Kakinoki <shunkakinoki@gmail.com>
2020-09-29 16:57:43 +0900

    perf: ⚡️ ini react hooks for segment manager

--- packages/ui/src/components/SegmentManager/SegmentManager.tsx
+++ packages/ui/src/components/SegmentManager/SegmentManager.tsx
@@ -5,31 +5,52 @@ import inRegions from "@segment/in-regions";
 import getConfig from "next/config";
 import * as React from "react";
 
+import isBrowser from "@sentrei/common/utils/isBrowser";
+
 const {publicRuntimeConfig} = getConfig();
 const segment_id = publicRuntimeConfig.SEGMENT_ID;
 
-const inCA = inRegions(["CA"]);
-const inEU = inRegions(["EU"]);
-const shouldRequireConsent = inRegions(["CA", "EU"]);
-
-const caDefaultPreferences = {
-  advertising: false,
-  marketingAndAnalytics: true,
-  functional: true,
-};
-const euDefaultPreferences = {
-  advertising: false,
-  marketingAndAnalytics: false,
-  functional: false,
-};
-
-const initialPreferences = inCA()
-  ? caDefaultPreferences
-  : inEU()
-  ? euDefaultPreferences
-  : undefined;
-
 export default function SegmentManager(): JSX.Element {
+  const [shouldRequireConsent, setShouldRequireConsent] = React.useState<
+    boolean
+  >(false);
+
+  const [initialPreferences, setnitialPreferences] = React.useState<
+    | {
+        advertising: boolean;
+        marketingAndAnalytics: boolean;
+        functional: boolean;
+      }
+    | undefined
+  >(undefined);
+
+  React.useEffect(() => {
+    if (isBrowser()) {
+      const inCA = inRegions(["CA"]);
+      const inEU = inRegions(["EU"]);
+      setShouldRequireConsent(inRegions(["CA", "EU"]));
+
+      const caDefaultPreferences = {
+        advertising: false,
+        marketingAndAnalytics: true,
+        functional: true,
+      };
+      const euDefaultPreferences = {
+        advertising: false,
+        marketingAndAnalytics: false,
+        functional: false,
+      };
+
+      const preferences = inCA()
+        ? caDefaultPreferences
+        : inEU()
+        ? euDefaultPreferences
+        : undefined;
+
+      setnitialPreferences(preferences);
+    }
+  }, []);
+
   return (
     <NoSsr>
       <ConsentManager
