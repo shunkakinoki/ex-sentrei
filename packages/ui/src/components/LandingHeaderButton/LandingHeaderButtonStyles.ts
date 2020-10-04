e9e6febb277a3759f7d6663466f2c5dec0d65a46
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-05 08:51:55 +0900

    perf: ⚡️ reini header button hover primary

--- packages/ui/src/components/LandingHeaderButton/LandingHeaderButtonStyles.ts
+++ packages/ui/src/components/LandingHeaderButton/LandingHeaderButtonStyles.ts
@@ -3,6 +3,9 @@ import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
 const LandingHeaderStyles = makeStyles((theme: Theme) =>
   createStyles({
     button: {
+      "&:hover": {
+        color: theme.palette.primary.main,
+      },
       margin: theme.spacing(1),
     },
   }),
