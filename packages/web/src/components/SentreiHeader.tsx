e14496a73d1fe7d9c6a3e25c77d68280ba857cae
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-05 02:10:09 +0900

    feat: 🎸 completely refactor header button & type

--- packages/web/src/components/SentreiHeader.tsx
+++ packages/web/src/components/SentreiHeader.tsx
@@ -10,12 +10,10 @@ type Props = Omit<Original, "logo">;
 export default function SentreiHeader({
   papercups,
   landingKey,
-  type = "default",
 }: Props): JSX.Element {
   return (
     <LandingHeader
       papercups={papercups}
-      type={type}
       landingKey={landingKey}
       logo={<LogoPicture />}
     />
