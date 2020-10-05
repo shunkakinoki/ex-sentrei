b6e8115bbc0f9a014b568a1d76e77b950cd3443c
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-05 16:29:09 +0900

    perf: ⚡️ refactor landing screen

--- packages/ui/src/components/LandingScreen/LandingScreen.tsx
+++ packages/ui/src/components/LandingScreen/LandingScreen.tsx
@@ -3,31 +3,17 @@ import * as React from "react";
 
 import LandingBanner from "@sentrei/ui/components/LandingBanner";
 import LandingFaq from "@sentrei/ui/components/LandingFaq";
-import LandingFeature, {
-  Props as LandingFeatureProps,
-} from "@sentrei/ui/components/LandingFeature";
 import LandingPricing from "@sentrei/ui/components/LandingPricing";
 import LandingProduct, {
   Props as LandingProductProps,
 } from "@sentrei/ui/components/LandingProduct";
-import LandingTestimonial, {
-  Props as LandingTestimonialProps,
-} from "@sentrei/ui/components/LandingTestimonial";
 
-export type Props = LandingProductProps &
-  LandingFeatureProps &
-  LandingTestimonialProps;
+export type Props = LandingProductProps;
 
 export default function LandingScreen({
   connectImg,
   dataImg,
   videoImg,
-  timeImg,
-  focusImg,
-  goalImg,
-  personOneImg,
-  personTwoImg,
-  personThreeImg,
 }: Props): JSX.Element {
   return (
     <>
@@ -37,14 +23,6 @@ export default function LandingScreen({
         connectImg={connectImg}
         dataImg={dataImg}
         videoImg={videoImg}
-      />
-      <Box p={3} />
-      <LandingFeature timeImg={timeImg} focusImg={focusImg} goalImg={goalImg} />
-      <Box p={3} />
-      <LandingTestimonial
-        personOneImg={personOneImg}
-        personTwoImg={personTwoImg}
-        personThreeImg={personThreeImg}
       />
       <Box p={3} />
       <LandingPricing />
