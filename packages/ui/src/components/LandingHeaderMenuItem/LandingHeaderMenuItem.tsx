7e8cfda19a397324bcd4f2ee93d42303b3241426
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-05 11:46:38 +0900

    perf: ⚡️ adjust landing header menu item to dark mode

--- packages/ui/src/components/LandingHeaderMenuItem/LandingHeaderMenuItem.tsx
+++ packages/ui/src/components/LandingHeaderMenuItem/LandingHeaderMenuItem.tsx
@@ -1,6 +1,7 @@
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 
+import {useTheme} from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import * as React from "react";
 
@@ -23,18 +24,34 @@ const LandingHeaderMenuItem = ({
   title,
   description,
 }: Props): JSX.Element => {
+  const theme = useTheme();
+
   return (
     <MuiListItem href={href} rel={rel} target={target}>
-      <ListItemIcon>{icon}</ListItemIcon>
+      <ListItemIcon
+        color={theme.palette.type === "light" ? "primary" : "textPrimary"}
+      >
+        {icon}
+      </ListItemIcon>
       <ListItemText
         primary={
-          <Typography align="left" variant="subtitle1" color="primary">
+          <Typography
+            align="left"
+            variant="subtitle1"
+            color={theme.palette.type === "light" ? "primary" : "textPrimary"}
+          >
             {title}
           </Typography>
         }
         secondary={
           description && (
-            <Typography component="span" variant="body2" color="textPrimary">
+            <Typography
+              component="span"
+              variant="body2"
+              color={
+                theme.palette.type === "light" ? "textPrimary" : "textSecondary"
+              }
+            >
               {description}
             </Typography>
           )
