b8b70fb64dcc33ee9edd3db5f7037eceefb3a0ec
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-06 15:50:36 +0900

    feat: 🎸 ini landing header menu event

--- packages/ui/src/components/LandingHeaderMenu/LandingHeaderMenu.tsx
+++ packages/ui/src/components/LandingHeaderMenu/LandingHeaderMenu.tsx
@@ -4,6 +4,7 @@ import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
 import clsx from "clsx";
 import * as React from "react";
 
+import {trackEvent} from "@sentrei/common/utils/segment";
 import LandingHeaderMenuPopover from "@sentrei/ui/components/LandingHeaderMenuPopover";
 
 import LandingHeaderMenuStyles from "./LandingHeaderMenuStyles";
@@ -11,11 +12,13 @@ import LandingHeaderMenuStyles from "./LandingHeaderMenuStyles";
 export interface Props {
   children: React.ReactNode;
   title: string;
+  type: "company" | "product" | "resources";
 }
 
 export default function LandingHeaderMenu({
   children,
   title,
+  type,
 }: Props): JSX.Element {
   const classes = LandingHeaderMenuStyles();
 
@@ -24,10 +27,12 @@ export default function LandingHeaderMenu({
   );
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>): void => {
     setAnchorEl(event.currentTarget);
+    trackEvent("Open Landing Menu", {type});
   };
 
   const handleClose = (): void => {
     setAnchorEl(null);
+    trackEvent("Close Landing Menu", {type});
   };
 
   const open = Boolean(anchorEl);
