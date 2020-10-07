7c6cde73080c4543b93e10528b324cdf2c6667b3
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-07 12:54:05 +0900

    perf: ⚡️ ini timeline item

--- packages/ui/src/components/LandingTimelineDot/LandingTimelineDot.tsx
+++ packages/ui/src/components/LandingTimelineDot/LandingTimelineDot.tsx
@@ -1,7 +1,11 @@
 import Box from "@material-ui/core/Box";
-import IconButton from "@material-ui/core/IconButton";
 
+import IconButton from "@material-ui/core/IconButton";
 import LooksOneIcon from "@material-ui/icons/LooksOne";
+import TimelineConnector from "@material-ui/lab/TimelineConnector";
+import TimelineContent from "@material-ui/lab/TimelineContent";
+import TimelineItem from "@material-ui/lab/TimelineItem";
+import TimelineSeparator from "@material-ui/lab/TimelineSeparator";
 import * as React from "react";
 
 import LandingTimelineDotStyles from "./LandingTimelineDotStyles";
@@ -10,10 +14,18 @@ export default function LandingTimelineDot(): JSX.Element {
   const classes = LandingTimelineDotStyles();
 
   return (
-    <Box display="flex" alignItems="center" justifyContent="center">
-      <IconButton className={classes.top} disabled>
-        <LooksOneIcon className={classes.icon} fontSize="large" />
-      </IconButton>
-    </Box>
+    <>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent />
+      </TimelineItem>
+      <Box display="flex" alignItems="center" justifyContent="center">
+        <IconButton className={classes.top} disabled>
+          <LooksOneIcon className={classes.icon} fontSize="large" />
+        </IconButton>
+      </Box>
+    </>
   );
 }
