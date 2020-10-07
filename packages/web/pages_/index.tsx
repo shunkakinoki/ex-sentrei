315b199c469d02ddcaa6c8f087046d47ccc359d1
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-07 10:02:44 +0900

    perf: ⚡️ refactor landing screen

--- packages/web/pages_/index.tsx
+++ packages/web/pages_/index.tsx
@@ -8,9 +8,6 @@ import LandingScreen from "@sentrei/ui/components/LandingScreen";
 import OneTap from "@sentrei/ui/components/OneTap";
 import SentreiFooter from "@sentrei/web/components/SentreiFooter";
 import SentreiHeader from "@sentrei/web/components/SentreiHeader";
-import ConnectPicture from "@sentrei/web/images/svg/ConnectPicture";
-import DataPicture from "@sentrei/web/images/svg/DataPicture";
-import VideoPicture from "@sentrei/web/images/svg/VideoPicture";
 
 const Index: NextPage = () => {
   const {user} = React.useContext(AuthContext);
@@ -23,11 +20,7 @@ const Index: NextPage = () => {
     <>
       <OneTap delay user={user} />
       <SentreiHeader landingKey="landing" />
-      <LandingScreen
-        connectImg={<ConnectPicture />}
-        dataImg={<DataPicture />}
-        videoImg={<VideoPicture />}
-      />
+      <LandingScreen />
       <SentreiFooter />
     </>
   );
