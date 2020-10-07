db4ab518a3bd571d3a46269159056c7063b51ce9
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-07 10:09:52 +0900

    feat: 🎸 ini icon for landing explore

--- packages/ui/src/components/LandingExplore/LandingExplore.tsx
+++ packages/ui/src/components/LandingExplore/LandingExplore.tsx
@@ -1,6 +1,8 @@
+import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
 import {useTheme} from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 import useTranslation from "next-translate/useTranslation";
 import * as React from "react";
 
@@ -10,14 +12,20 @@ export default function LandingOutline(): JSX.Element {
 
   return (
     <Container maxWidth="md">
-      <Typography
-        align="center"
-        component="h6"
-        variant="h6"
-        color={theme.palette.type === "light" ? "primary" : "inherit"}
-      >
-        {t("index:explore.title")}
-      </Typography>
+      <Box display="flex" alignItems="center" justifyContent="center">
+        <Typography
+          align="center"
+          component="h6"
+          variant="h6"
+          color={theme.palette.type === "light" ? "primary" : "inherit"}
+        >
+          {t("index:explore.title")}
+        </Typography>
+        <ArrowDropDownIcon
+          fontSize="large"
+          color={theme.palette.type === "light" ? "primary" : "inherit"}
+        />
+      </Box>
     </Container>
   );
 }
