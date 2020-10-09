5fd9f5a7ccb08e198f23c6bd2946f44dcd81daf8
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-09 15:37:04 +0900

    perf: ⚡️ ini footer for sales & support

--- packages/web/pages_/sales.tsx
+++ packages/web/pages_/sales.tsx
@@ -5,6 +5,7 @@ import * as React from "react";
 import AuthContext from "@sentrei/common/context/AuthContext";
 
 import SalesScreen from "@sentrei/ui/components/SalesScreen";
+import SentreiFooter from "@sentrei/web/components/SentreiFooter";
 import SentreiHeader from "@sentrei/web/components/SentreiHeader";
 
 const Sales: NextPage = () => {
@@ -18,6 +19,7 @@ const Sales: NextPage = () => {
     <>
       <SentreiHeader papercups={false} landingKey="sales" />
       <SalesScreen />
+      <SentreiFooter />
     </>
   );
 };
