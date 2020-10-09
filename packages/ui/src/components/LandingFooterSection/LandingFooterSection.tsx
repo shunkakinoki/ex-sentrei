8fc90e7c921be218b59d4b1b4a5f95ab3a469c85
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-09 13:44:29 +0900

    fix: 🐛 refactor landing footer section for support

--- packages/ui/src/components/LandingFooterSection/LandingFooterSection.tsx
+++ packages/ui/src/components/LandingFooterSection/LandingFooterSection.tsx
@@ -27,9 +27,6 @@ export default function LandingFooterSection(): JSX.Element {
           <MuiLink href="/media">
             <Typography gutterBottom>{t("footer:footer.media")}</Typography>
           </MuiLink>
-          <MuiLink href="/support">
-            <Typography gutterBottom>{t("footer:footer.support")}</Typography>
-          </MuiLink>
         </Grid>
         <Grid item xs={12} sm={3} md={3}>
           <Typography variant="h6" gutterBottom>
@@ -43,6 +40,9 @@ export default function LandingFooterSection(): JSX.Element {
           </MuiLink>
           <MuiLink href="/pricing">
             <Typography gutterBottom>{t("footer:footer.pricing")}</Typography>
+          </MuiLink>
+          <MuiLink href="/support">
+            <Typography gutterBottom>{t("footer:footer.support")}</Typography>
           </MuiLink>
         </Grid>
         <Grid item xs={12} sm={3} md={3}>
