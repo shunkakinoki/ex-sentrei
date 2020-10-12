ae714c595f0b2fec2f22db9a8de7456b9584e35c
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-12 15:46:16 +0900

    perf: ⚡️ refactor button default color

--- packages/ui/src/components/LandingHeaderButton/LandingHeaderButtonStyles.ts
+++ packages/ui/src/components/LandingHeaderButton/LandingHeaderButtonStyles.ts
@@ -3,6 +3,7 @@ import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
 const LandingHeaderStyles = makeStyles((theme: Theme) =>
   createStyles({
     button: {
+      color: theme.palette.text.primary,
       "&:hover": {
         color: theme.palette.primary.main,
       },
